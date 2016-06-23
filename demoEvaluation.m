bc27c2665de7f739eb501304544d85dba08c5a03
Hang <hangzhao@mit.edu>
2016-06-22 21:15:03 -0400

    add accuracy measure to demoEvaluation

--- demoEvaluation.m
+++ demoEvaluation.m
@@ -4,12 +4,12 @@ close all; clc; clear;
 
 %% Options and paths
 VERBOSE = 0;    % to show individual image results, set it to 1
+
 % path to image(.jpg), prediction(.png) and annotation(.png)
+% *NOTE: change these paths while evaluating your own predictions
 pathImg = fullfile('sampleData', 'images');
-%pathPred = fullfile('sampleData', 'predictions');
-%pathAnno = fullfile('sampleData', 'annotations');
-pathPred = '/data/vision/oliva/scenedataset/segmentation_hang/SceneSegmentation/object150/visualization_test/';
-pathAnno = '/data/vision/oliva/scenedataset/segmentation_hang/ADE20K/trainval/anno384_mirror/';
+pathPred = fullfile('sampleData', 'predictions');
+pathAnno = fullfile('sampleData', 'annotations');
 
 addpath(genpath('evaluationCode/'));
 addpath(genpath('visualizationCode'));
@@ -26,6 +26,9 @@ load('color150.mat');
 cnt=0;
 area_intersection = double.empty;
 area_union = double.empty;
+pixel_accuracy = double.empty;
+pixel_correct = double.empty;
+pixel_labeled = double.empty;
 
 % main loop
 filesPred = dir(fullfile(pathPred, '*.png'));
@@ -54,18 +57,18 @@ for i = 1: numel(filesPred)
     fprintf('Evaluating %d/%d...\n', cnt, numel(filesPred));
     [area_intersection(:,cnt), area_union(:,cnt)] = intersectionAndUnion(imPred, imAnno, numClass);
 
+    % compute pixel-wise accuracy
+    [pixel_accuracy(i), pixel_correct(i), pixel_labeled(i)] = pixelAccuracy(imPred, imAnno);
+
     % Verbose: show indivudual image results
     if (VERBOSE)
         % read image
         fileImg = fullfile(pathImg, strrep(filesPred(i).name, '.png', '.jpg'));
         im = imread(fileImg);
         
-        % compute pixel-wise accuracy
-        [pixel_accuracy, ~, ~]= pixelAccuracy(imPred, imAnno);
-        fprintf('[%s] Pixel-wise accuracy: %2.2f%%\n', fileImg, pixel_accuracy*100.);
-        
         % plot result
         plotResult(im, imPred, imAnno, objectNames, colors, fileImg);
+        fprintf('[%s] Pixel-wise accuracy: %2.2f%%\n', fileImg, pixel_accuracy(i)*100.);
         waitforbuttonpress;
     end   
 end
@@ -73,9 +76,11 @@ end
 %% Summary
 IoU = sum(area_intersection,2)./sum(eps+area_union,2);
 mean_IoU = mean(IoU);
+accuracy = sum(pixel_correct)/sum(pixel_labeled);
 
 fprintf('==== Summary IoU ====\n');
 for i = 1:numClass
     fprintf('%3d %16s: %.4f\n', i, objectNames{i}, IoU(i));
 end
 fprintf('Mean IoU over %d classes: %.4f\n', numClass, mean_IoU);
+fprintf('Pixel-wise Accuracy: %2.2f%%\n', accuracy*100.);
